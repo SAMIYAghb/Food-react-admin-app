3f252ab8c6b6498f56e5a64f3881dfdef907983a
samiya <samii-1989@live.com>
2023-12-03 20:02:17 +0100

    deploy github2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
